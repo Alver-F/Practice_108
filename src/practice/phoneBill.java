969b6d9067d77302a133cbf854592f1e7da5b5c3
Alver-F <rbca22108.rcca@gmail.com>
2024-02-06 17:59:36 +0530

    Master Commit

---
+++ src/practice/phoneBill.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class phoneBill {
+	String acct_num;
+	int num_units;
+	phoneBill(String acct_num,int num_units){
+		this.acct_num=acct_num;
+		this.num_units=num_units;
+	}
+
+}
